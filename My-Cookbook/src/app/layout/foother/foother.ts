b7ac89d30089988df57f3054542585c1548c6a70
SilviyaIvanova91 <99129696+SilviyaIvanova91@users.noreply.github.com>
2026-04-07 14:31:09 +0300

    Add My-Cookbook as regular folder

---
+++ My-Cookbook/src/app/layout/foother/foother.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foother',
+  imports: [],
+  templateUrl: './foother.html',
+  styleUrl: './foother.css',
+})
+export class FootherComponent {}
