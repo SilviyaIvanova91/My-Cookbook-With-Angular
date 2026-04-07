558e34d292e6d301fd150827177e43d156709dff
SilviyaIvanova <silviya.yordanova91@gmail.com>
2026-04-07 14:36:43 +0300

    Delete src directory

--- src/app/layout/foother/foother.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-foother',
-  imports: [],
-  templateUrl: './foother.html',
-  styleUrl: './foother.css',
-})
-export class FootherComponent {}
